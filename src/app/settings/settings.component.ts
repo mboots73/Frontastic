c8ffbedfe70f14c1eb3d4952e03cc03a7efc674d
Jouw Naam <mboots@sogyo.nl>
2017-05-23 11:19:13 +0200

    made profile page with image and name + made settings page with form/input fields

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  user =  {
+    name: '',
+    email: '',
+    username: '',
+    password: '',
+    profilepicture: ''
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onSubmit(form: NgForm) {
+    console.log(this.user);
   }
 
 }
