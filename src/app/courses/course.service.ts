bc1229d6c9e8bd3f7a14f4bb3c6c39a4724ca50f
Jouw Naam <mboots@sogyo.nl>
2017-05-29 13:23:01 +0200

    changed name of image files for consistency

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -7,13 +7,12 @@ export class CourseService {
 
     new Course('HTML', 'Language for creating web-pages','/assets/html.png', '/courses/html', '50'),
     new Course('CSS', 'Language to style web-pages', '/assets/css.png', '/courses/css', '0'),
-    new Course('JavaScript', 'Language to make web-pages interactive', '/assets/javascript-geel.png', '/courses/javascript',  '0'),
+    new Course('JavaScript', 'Language to make web-pages interactive', '/assets/javascript.png', '/courses/javascript',  '0'),
     new Course('jQuery', 'A JavaScript library', '/assets/jquery.png', '/courses/jquery',  '0'),
-    new Course('Angular.js', 'A JavaScript MVC framework', '/assets/angular.png', '/courses/angularjs', '0'),
+    new Course('Angular.js', 'A JavaScript MVC framework', '/assets/angularjs.png', '/courses/angularjs', '0'),
     new Course('Three.js', 'A JavaScript 3D library', '/assets/threejs.png', '/courses/threejs', '0'),
-    new Course('Ember.js', 'A JavaScript MVVM framework', '/assets/Emberjs.png', '/courses/emberjs',  '0'),
-    new Course('Bootstrap', 'A framework with HTML/CSS templates', '/assets/bootstrap.png', '/courses/bootstrap',  '0'),
-    // new Course('Add a new course', ' ', '/assets/newcourse.png', '/courses/newcourse', '0')
+    new Course('Ember.js', 'A JavaScript MVVM framework', '/assets/emberjs.png', '/courses/emberjs',  '0'),
+    new Course('Bootstrap', 'A framework with HTML/CSS templates', '/assets/bootstrap.png', '/courses/bootstrap',  '0')
   ];
   constructor() { }
 
@@ -22,8 +21,5 @@ export class CourseService {
   }
   addCourse(hoi) {
   this.courses.push(hoi);
-  console.log(hoi + 'from course')
-  hoi = '';
-  console.log(hoi + 'from course')
   }
 }
