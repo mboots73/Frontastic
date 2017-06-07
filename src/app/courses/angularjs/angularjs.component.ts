89f732b811441c09e2aa79bff33c8e9482ca0f79
Jouw Naam <mboots@sogyo.nl>
2017-06-07 13:20:13 +0200

    26787 (partial) applied to all courses: data is fetched from database and now also shows when image is updated from normal to enrolled to finished. Stays changed after refreshing the page as well.

--- src/app/courses/angularjs/angularjs.component.ts
+++ src/app/courses/angularjs/angularjs.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FirebaseService } from '../../firebase.service';
 @Component({
   selector: 'app-angularjs',
   templateUrl: './angularjs.component.html'
 })
 export class AngularjsComponent implements OnInit {
 
-  constructor() { }
+  courses:any
+  angular:any;
+  name:any;
+  category:any;
+  description:any;
+  lesson1:any;
+  lesson2:any;
+  lesson1url:any;
+  image:any;
+    constructor(private fs: FirebaseService) {
+      this.fs.getCourses().subscribe(courses => {
+        this.courses = courses;
+        this.angular = this.courses[0];
+    this.name = this.angular.name;
+    this.category = this.angular.category;
+    this.description = this.angular.explanation;
+    this.lesson1 = this.angular.lesson1;
+    this.lesson2 = this.angular.lesson2;
+    this.lesson1url = this.angular.lesson1url;
+    this.image = this.angular.imageurl;
+      });
 
+  }
   ngOnInit() {
 
-}
+  }
 }
--- src/app/courses/css/css.component.ts
+++ src/app/courses/css/css.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { FirebaseService } from '../../firebase.service';
 @Component({
   selector: 'app-css',
   templateUrl: './css.component.html'
 })
 export class CssComponent implements OnInit {
 
-  constructor() { }
+  courses:any
+  css:any;
+  name:any;
+  category:any;
+  description:any;
+  lesson1:any;
+  lesson2:any;
+  lesson1url:any;
+  image:any;
+    constructor(private fs: FirebaseService) {
+      this.fs.getCourses().subscribe(courses => {
+        this.courses = courses;
+        this.css = this.courses[2];
+    this.name = this.css.name;
+    this.category = this.css.category;
+    this.description = this.css.explanation;
+    this.lesson1 = this.css.lesson1;
+    this.lesson2 = this.css.lesson2;
+    this.lesson1url = this.css.lesson1url;
+    this.image = this.css.imageurl;
+      });
 
-  ngOnInit() {
   }
+  ngOnInit() {
 
+  }
 }
